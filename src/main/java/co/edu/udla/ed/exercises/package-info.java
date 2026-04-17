07f82e14932fe6e77f8a257fc037a319c91b2e6c
Sergio Majé <smajefranco@gmail.com>
2026-04-16 21:41:58 -0500

    📝 docs(exercises): document the student practice package

---
+++ src/main/java/co/edu/udla/ed/exercises/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Exercise templates and student-facing practice material.
+ *
+ * <p>This package can host guided TODO-based activities without affecting the
+ * contract tests that validate the main implementations.</p>
+ */
+package co.edu.udla.ed.exercises;
